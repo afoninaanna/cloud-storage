78994b2bcf551ebb979964c6ff3979e302da7601
afoninaanna <afoninaa903@gmail.com>
2023-03-13 19:38:45 +0400

    add: avatar by the server

--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -5,9 +5,11 @@ const fileController = require('../controllers/fileController');
 
 router.post('', authMiddleware, fileController.createDir);
 router.post('/upload', authMiddleware, fileController.uploadFile);
+router.post('/avatar', authMiddleware, fileController.uploadAvatar);
 router.get('', authMiddleware, fileController.fetchFiles);
 router.get('/download', authMiddleware, fileController.downloadFile);
 router.get('/search', authMiddleware, fileController.searchFiles);
 router.delete('/', authMiddleware, fileController.deleteFile);
+router.delete('/avatar', authMiddleware, fileController.deleteAvatar);
 
 module.exports = router;
